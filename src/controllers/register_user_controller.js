6fc42ad51c9d5fa5c7428af9be1365bbd7ebf842
ctechitdev <ctech.itdev@gmail.com>
2023-06-13 14:45:26 +0700

    aiy

--- src/controllers/register_user_controller.js
+++ src/controllers/register_user_controller.js
@@ -1,43 +1,29 @@
-const request = require('express/lib/request');
-const connected = require('../../setting/connect');
+const request = require("express/lib/request");
+const connected = require("../../setting/connect");
 
-const queries = require('../queries/register_user_query');
-
+const queries = require("../queries/register_user_query");
 
 const register_user = (request, respond) => {
+  // respond.status(200).json("API Register User");
 
+  const { full_name, gender, phone_number, pass_word } = request.body;
 
-    // respond.status(200).json("API Register User");
-
-    const random_password = "123452";
-
-    const{full_name,gender,phone_number} = request.body;
-
-    connected.query(queries.checkphone , [phone_number] , (error , results)=>{
-      if (results.rows.length){
-
-        respond.send("this number is exit");
-
-      }else{ 
-
-      
-      connected.query(queries.adduser, [full_name, gender,phone_number,random_password] , (error , results)=>{
-     
-        respond.send("success full");
-  
- 
-    });
-
-
-      }
-    });
-
-      
-
-
-  };
+  connected.query(queries.checkphone, [phone_number], (error, results) => {
+    if (results.rows.length) {
+      respond.send("ເບີນີ້ໄດ້ລົງທະບຽນແລ້ວ");
+    } else {
+      connected.query(
+        queries.adduser,
+        [full_name, gender, phone_number, pass_word],
+        (error, results) => {
+          if (error) throw error;
+          respond.send("ລົງທະບຽນສຳເລັດ");
+        }
+      );
+    }
+  });
+};
 
 module.exports = {
-    register_user,
-    
-}
+  register_user,
+};
