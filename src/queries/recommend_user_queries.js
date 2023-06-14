60456bb0953ffd89ab84d1d7413273664cb01bdd
thaksin-soft <thaksin.kvs@gmail.com>
2023-06-14 10:29:55 +0700

    update

--- src/queries/recommend_user_queries.js
+++ src/queries/recommend_user_queries.js
@@ -1,5 +1,14 @@
 // command sql auto register recommend number
 
+const recommend_number = "INSERT INTO tbl_lottery_recommend_number(user_external_id,lottery_recommend_number,lrn_register_date) VALUES($1, $2, now())";
+
 
 
 // command sql show single recommend number
+const show_recommend_number = "select user_external_id,lottery_recommend_number from tbl_lottery_recommend_number where user_external_id = $1";
+
+
+module.exports = {
+    recommend_number,
+    show_recommend_number
+};
