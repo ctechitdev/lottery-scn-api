f90d1e4db3025be2562187639abe4624bad7c66d
ctechitdev <ctech.itdev@gmail.com>
2023-06-14 12:05:36 +0700

    aiy

--- src/controllers/password_reset_controller.js
+++ src/controllers/password_reset_controller.js
@@ -5,9 +5,26 @@ const queries = require('../queries/password_reset_query');
 
 
 const reset_password = (request, respond) => {
+   // respond.status(200).json("API reset password");   
+   
+   const { phone_number,password } = request.body;
+ 
+   connected.query(queries.getidreset, [phone_number], (error, results) => {
+     const checkpass = !results.rows.length;
+     if (checkpass) {
+       respond.send("ບໍ່ມີໃນລະບົບ");
+     }else{
+     connected.query(queries.setpassword, [phone_number,password], (error, results) => {
+       if (error) throw error;
+       respond.status(200).send("ປ່ຽນລະຫັດສຳເລັດ");
+     
+     });
+    }
+   });
 
 
-    respond.status(200).json("API reset password");
+
+
 
 
 }
