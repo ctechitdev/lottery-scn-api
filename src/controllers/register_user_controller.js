70884f41b6797940ba3c4a1c9da38ae38780ba06
ctechitdev <ctech.itdev@gmail.com>
2023-06-18 12:17:58 +0700

    aiy

--- src/controllers/register_user_controller.js
+++ src/controllers/register_user_controller.js
@@ -6,13 +6,16 @@ const queries = require("../queries/register_user_query");
 const register_user = (request, respond) => {
   // respond.status(200).json("API Register User");
 
-  const { full_name, gender, phone_number, pass_word } = request.body;
+  const { full_name, gender, phone_number, pass_word ,checkpassword } = request.body;
 
 //check number
 
-  connected.query(queries.checkphone, [phone_number], (error, results) => {
-    if (results.rows.length) {
+  connected.query(queries.checkphone, [phone_number,checkpassword], (error, results) => {
+    
+   if (results.rows.length) {
       respond.send("ເບີນີ້ໄດ້ລົງທະບຽນແລ້ວ!");
+    }else if(checkpassword !== pass_word){
+      respond.send("ລະຫັດບໍ່ຕົງ")
     } else {
       //add user
       connected.query(
