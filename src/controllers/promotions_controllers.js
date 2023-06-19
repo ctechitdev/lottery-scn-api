085c8f838ea6ab93ff8723954a679845000fb894
ctechitdev <ctech.itdev@gmail.com>
2023-06-19 12:21:50 +0700

    OB T19 12:21

--- src/controllers/promotions_controllers.js
+++ src/controllers/promotions_controllers.js
@@ -14,15 +14,19 @@ const secretkey = "CtectLottery";
 // ຄິດໄລ່ຫັກຍອດເງິນຄືນ
 const promotions_cash_back = (request, respond) => {
     const { cbpo_id } = request.body;
-    connected.query(queries.promotions_cash_back, [cbpo_id], (error, results) => {
+    
+    connected.query(queries.promotions_cash_back_ById, [cbpo_id], (error, results) => {
         if (error) throw error;
-        if (results.rows.length) {
-            respond.status(200).json(results.rows);
+        if(results.rows.length) {
+                respond.status(200).json(results.rows);
         } else {
-            respond.status(200).json("API Show total Cash back price");
+            connected.query(queries.promotions_cash_back, (error, results)=>{
+            if (error) throw error;
+            respond.status(200).json(results.rows);
         }
-    });
+    )}});
 };
+
 
  
 
--- src/queries/promotions_queries.js
+++ src/queries/promotions_queries.js
@@ -1,8 +1,10 @@
-const promotions_cash_back = "select * from tbl_cash_back_promotion where cbpo_id = $1";
+const promotions_cash_back = "select * from tbl_cash_back_promotion ";
+const promotions_cash_back_ById = "select * from tbl_cash_back_promotion where cbpo_id = $1";
 
 
 
 module.exports = {
     promotions_cash_back,
+    promotions_cash_back_ById,
   };
   
