86f6113a3d514ecad35c12386b40c448f66c26a8
ctechitdev <ctech.itdev@gmail.com>
2023-06-21 12:50:06 +0700

    aiy 21 /6

--- src/controllers/password_reset_controller.js
+++ src/controllers/password_reset_controller.js
@@ -1,38 +1,38 @@
-const request = require('express/lib/request');
-const connected = require('../../setting/connect');
+const request = require("express/lib/request");
+const connected = require("../../setting/connect");
 
-const queries = require('../queries/password_reset_query');
+const queries = require("../queries/password_reset_query");
 
+const bcrypt = require("bcrypt");
 
-const reset_password = (request, respond) => {
-   // respond.status(200).json("API reset password");   
-   
-   const random_number = Math.floor(Math.random()  * 3473642) * 18 ;
-  
+const reset_password = async (request, respond) => {
+  // respond.status(200).json("API reset password");
 
-   const { phone_number } = request.body;
- 
-   connected.query(queries.getidreset, [phone_number], (error, results) => {
-     const checkpass = !results.rows.length;
-     if (checkpass) {
-       respond.send("ບໍ່ມີໃນລະບົບ");
-     }else{
-     connected.query(queries.setpassword, [phone_number,random_number], (error, results) => {
-       if (error) throw error;
-       respond.status(200).send("ປ່ຽນລະຫັດສຳເລັດ");
-     
-     });
+  const random_number = Math.floor(Math.random() * 3473642) * 18;
+
+  const { phone_number } = request.body;
+
+  const newPassword = random_number.toString();
+
+  const encryptPassword = await bcrypt.hash(newPassword, 10);
+
+  connected.query(queries.checkphone, [phone_number], (error, results) => {
+    const checkpass = !results.rows.length;
+    if (checkpass) {
+      respond.send("ບໍ່ມີໃນລະບົບ");
+    } else {
+      connected.query(
+        queries.setpassword,
+        [phone_number, encryptPassword],
+        (error, results) => {
+          if (error) throw error;
+          respond.status(200).send("ປ່ຽນລະຫັດສຳເລັດ");
+        }
+      );
     }
-   });
-
-
-
-
-
-
-}
-
+  });
+};
 
 module.exports = {
-    reset_password,
-}
+  reset_password,
+};
--- src/queries/password_reset_query.js
+++ src/queries/password_reset_query.js
@@ -1,7 +1,7 @@
-const getidreset = "select * from  tbl_user_external where phone_number =$1" ;
+const checkphone = "select * from  tbl_user_external where phone_number =$1" ;
  const setpassword = "update tbl_user_external set pass_word =$2  where phone_number = $1";
 
  module.exports ={
-    getidreset,
+   checkphone,
     setpassword,
  };
