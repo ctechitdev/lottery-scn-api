73ae56de2df49f6ad2a974a73e7f3bc637004a4d
ctechitdev <ctech.itdev@gmail.com>
2023-06-21 12:56:26 +0700

    aiy

--- src/controllers/password_reset_controller.js
+++ src/controllers/password_reset_controller.js
@@ -9,10 +9,12 @@ const reset_password = async (request, respond) => {
   // respond.status(200).json("API reset password");
 
   const random_number = Math.floor(Math.random() * 3473642) * 18;
+  
+  const newPassword = random_number.toString();
 
   const { phone_number } = request.body;
 
-  const newPassword = random_number.toString();
+  
 
   const encryptPassword = await bcrypt.hash(newPassword, 10);
 
