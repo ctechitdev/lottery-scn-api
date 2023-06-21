97d54daabac3c73a62323593ed00ce7f98c1a63c
ctechitdev <ctech.itdev@gmail.com>
2023-06-21 15:52:18 +0700

    aiy

--- src/controllers/buy_lottery_controllers.js
+++ src/controllers/buy_lottery_controllers.js
@@ -4,49 +4,48 @@ const connected = require("../../setting/connect");
 const queries = require("../queries/buy_lottery_queries");
 
 // call json web token
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // key use for decript and encrype JWT
 const secretkey = "CtectLottery";
 
-
 // ກວດສອບໂຄຕ້າ ແລະ ໂປຣໂມຊັ້ນ ຂອງເລກສ່ຽງ
 const check_lottery_quota_promotion = (request, respond) => {
-    // 1.ກວດສອບໂຄຕ້າເລກສ່ຽງ
-    // 2.ຄິດໄລ່ຫັກຍອດຄືນເງິນ
-    // 3.ສະແດງຍອດທີ່ຕ້ອງຈ່າຍ
+  // 1.ກວດສອບໂຄຕ້າເລກສ່ຽງ
+  // 2.ຄິດໄລ່ຫັກຍອດຄືນເງິນ
+  // 3.ສະແດງຍອດທີ່ຕ້ອງຈ່າຍ
 
-    respond
-        .status(200)
-        .json("API Check lottery number litmit price and promotion cash back");
+  respond
+    .status(200)
+    .json("API Check lottery number litmit price and promotion cash back");
 };
+
 // ຊຳລະເງິນ
 const payment_lottery_bank = (request, respond) => {
-    respond.status(200).json("API insert data payment");
+  respond.status(200).json("API insert data payment");
 };
 
 // ປະຫວັດການຊື້ເລກ
-const history_bought_history = (request, respond) => {
-    const { bank_id } = request.body;
-    connected.query(queries.addlottery, [bank_id], (error, results) => {
-
+const history_bought_history = async (request, respond) => {
+  jwt.verify(request.token, secretkey, (error, rstoken) => {
+    const bbi_id = rstoken.id;
+    if (error) {
+      respond.status(201).json("token Expire");
+    } else {
+      connected.query(queries.showhistory, [bbi_id], (error, results) => {
+        if (error) throw error;
         if (results.rows.length) {
-            respond.status(201).send("create successfully!");
+          respond.status(200).json(results.rows);
         } else {
-            const { lottery_number, lottery_price } = request.body;
-            connected.query(
-                queries.addlotterydetail, [lottery_number, lottery_price],
-                (error, results) => {
-                    if (error) throw error;
-                    respond.status(201).send("create successfully!");
-                }
-            );
+          respond.status(200).send("no item");
         }
-    });
+      });
+    }
+  });
 };
 
 module.exports = {
-    check_lottery_quota_promotion,
-    payment_lottery_bank,
-    history_bought_history,
+  check_lottery_quota_promotion,
+  payment_lottery_bank,
+  history_bought_history,
 };
