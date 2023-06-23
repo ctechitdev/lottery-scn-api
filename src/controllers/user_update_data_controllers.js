4e16c5c7afbdb4da014113213bbb7c9eca1e1111
thaksin-soft <thaksin.kvs@gmail.com>
2023-06-23 18:17:59 +0700

    thaksin-23062023

--- src/controllers/user_update_data_controllers.js
+++ src/controllers/user_update_data_controllers.js
@@ -17,55 +17,33 @@ const secretkey = "CtectLottery";
 // ສະແດງຂໍ້ມູນຜູ້ໃຊ
 const show_user_data = (request, respond) => {
 
-<<<<<<< HEAD
     jwt.verify(req.token, secretkey, (err, rstoken) => {
-
-        const id = rstoken.id;
-
-       if (err) {
-            res.sendStatus(403);
+        if (err) {
+            res.status(200).json("token expire");
         } else {
-    connected.query(queries.show_user_data, [id], (error, results) => {
-        if (error) throw error;
-        if (results.rows.length) {
-            respond.status(200).json(results.rows);
-        } else {
-            respond.status(200).send("no users");
-        }
-    })
-        }
-});
-=======
-    const { id } = request.body;
-
-    console.log(id);
     
 
-connected.query(queries.show_user_data,[id],(error, results)=> {
-    if(error) throw error;
-    if(results.rows.length){
-        respond.status(200).json(results.rows);
-    }else{
-        respond.status(200).send("no users");
-    }
-})    
->>>>>>> f08329d9d3b56e3b435e231dfcd06df76f958c80
-
-
-
+        connected.query(queries.show_user_data,[id],(error, results)=> {
+            if(error) throw error;
+            if(results.rows.length){
+                respond.status(200).json(results.rows);
+            }else{
+                respond.status(200).send("no users");
+            }
+            }) ;
+        }
+        }); 
     ///respond.status(200).json("API single User data");
-
-
 }
 
 //ແກ້ໄຂຂໍ້ມູນຜູ້ໃຊ້ 
 const update_user_data = (request, respond) => {
 
-<<<<<<< HEAD
     jwt.verify(req.token, secretkey, (err, rstoken) => {
-
-    const id = rstoken.id;
-    const { gender, full_name, pass_word } = request.body;
+        if (err) {
+            res.status(200).json("token expire");
+        } else {
+    const {id, gender, full_name, pass_word } = request.body;
 
     connected.query(queries.update_user_data, [id, gender, full_name, pass_word], (error, results) => {
             if (error) throw error;
@@ -75,36 +53,32 @@ const update_user_data = (request, respond) => {
                 respond.status(200).json("update error");
             }
         })
+    }
     });
         //respond.status(200).json("API update User single data");
-=======
-    const { id,gender,full_name, pass_word , user_status} = request.body;
-
-    connected.query(queries.update_user_data,[id,gender,full_name, pass_word],(error,results)=> {
-        if(error) throw error;
-        if(results.rowCount == 1) {
-            respond.status(200).json("update done");
-        }else{
-            respond.status(200).json("update error");
-        }
-    })
-    //respond.status(200).json("API update User single data");
->>>>>>> f08329d9d3b56e3b435e231dfcd06df76f958c80
 }
 
 
 const join_recommend_number_sub_user = (request, respond) => {
 
-    const { id,gender,full_name, pass_word , user_status} = request.body;
+    const {id, gender, full_name, pass_word } = request.body;
 
-    connected.query(queries.update_user_data,[id,gender,full_name, pass_word , user_status],(error,results)=> {
-        if(error) throw error;
-        if(results.rowCount == 1) {
-            respond.status(200).json("update done");
-        }else{
-            respond.status(200).json("update error");
-        }
-    })
+    jwt.verify(req.token, secretkey, (err, rstoken) => {
+        if (err) {
+            res.status(200).json("token expire");
+        } else {
+
+
+    connected.query(queries.update_user_data, [id, gender, full_name, pass_word], (error, results) => {
+            if (error) throw error;
+            if (results.rowCount == 1) {
+                respond.status(200).json("update done");
+            } else {
+                respond.status(200).json("update error");
+            }
+        })
+    }
+    });
     //respond.status(200).json("API update User single data");
 }
 
