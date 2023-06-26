fdfd3ef271289adcbde19d07326a8024ebc111fd
itdevctech <itdevctech@gmail.com>
2023-06-26 16:29:58 +0700

    newupdatebilly26062023

--- src/controllers/password_reset_controller.js
+++ src/controllers/password_reset_controller.js
@@ -8,7 +8,7 @@ const bcrypt = require("bcrypt");
 const reset_password = async(request, respond) => {
     // respond.status(200).json("API reset password");
 
-    const random_number = Math.floor(Math.random() * 3473642) * 18;
+    const random_number = Math.floor(Math.random() * 1000000) ;
 
     const newPassword = random_number.toString();
 
