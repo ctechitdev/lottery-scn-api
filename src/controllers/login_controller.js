e50802469fd3ee0de6155fccd54b97daa0ba7b2d
anon <sd77553141@gmail.com>
2023-08-04 13:21:25 +0700

    aiy

--- src/controllers/login_controller.js
+++ src/controllers/login_controller.js
@@ -2,33 +2,32 @@ const request = require('express/lib/request');
 const connected = require('../../setting/connect');
 
 const queries = require('../queries/login_query');
-
-// call bcrype package
+ 
 const bcrypt = require('bcrypt');
 
- 
+const jwt = require('jsonwebtoken');
 
 const login = (request, respond) => {
 
-    const { registration_token } = request.body;
+    const { phone_number,otp } = request.body;
 
-    connected.query(queries.login_check, [otp], (error, results) => {
+    connected.query(queries.login_check, [phone_number], (error, results) => {
         if (error) throw error;
         if (results.rows.length) {
             const users = results.rows[0];
 
 
-            bcrypt.compare(registration_token, users.otp, (error, isMatch) => {
+             //respond.status(200).json(users.otp)
+ 
+                if (otp === users.otp ) {
+                    const accessToken = jwt.sign({ id: users.otp }, secretkey  );
 
-                if (isMatch) {
-                  
-                    respond.status(200).send("login success");
-
+                    respond.status(200).json([{ accessToken }])
 
                 } else {
                     respond.status(200).send("ລະຫັດຜ່ານບໍ່ຖືກຕ້ອງ");
                 }
-            })
+         
 
         } else {
             respond.status(200).send("ບໍ່ພົບຂໍ້ມູນຜູ້ໃຊ້");
--- src/queries/login_query.js
+++ src/queries/login_query.js
@@ -1,5 +1,4 @@
-const login_check = "SELECT a.id, firstname, lastname, a.phone, email, dob, gender,otp, image, registration_token"+
-"FROM customer a left join otp b on a.id = b.id where otp = $1 ";
+const login_check = "SELECT a.id, otp  FROM customer a left join otp b on a.id = b.id where a.phone = $1 ";
 
 
 module.exports = {
